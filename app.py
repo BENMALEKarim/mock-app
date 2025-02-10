c8c50774644aa5c7b23b196d9a0395d6c62d193b
BENMALEKarim <fk_benmalek@esi.dz>
2025-02-10 14:14:12 +0100

    update app

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Hello Folks. My awesome application message: ' + os.environ.get('MESSAGE')
+    return 'Hello CoP. My awesome application message: ' + os.environ.get('MESSAGE')
 
 app.run(debug=True, host='0.0.0.0', port=5000)

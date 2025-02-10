8aa452c5fef242bddac62b626250cbe6ff3eacba
BENMALEKarim <fk_benmalek@esi.dz>
2025-02-10 14:37:10 +0100

    feature: add Remi to list

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Hello Pierre, Karim and CoP devops: ' + os.environ.get('MESSAGE')
+    return 'Hello Pierre, Remi, Karim and CoP Devops: ' + os.environ.get('MESSAGE')
 
 app.run(debug=True, host='0.0.0.0', port=5000)

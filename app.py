54a254fc1681b38977b082d623de30cd71336f5d
BENMALEKarim <fk_benmalek@esi.dz>
2025-02-11 14:34:06 +0100

    fix: substitute Remi by Hakim

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Hello Pierre, Remi, Karim and CoP Devops: ' + os.environ.get('MESSAGE')
+    return 'Hello Pierre, Hakim, Karim and CoP Devops: ' + os.environ.get('MESSAGE')
 
 app.run(debug=True, host='0.0.0.0', port=5000)
